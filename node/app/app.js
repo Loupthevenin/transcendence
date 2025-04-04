04e28cfdf90b8bd343d7f9239e6f86e4203a56e0
Loup Thevenin <loup.thevenin.1@gmail.com>
2025-04-04 15:43:17 +0200

    up dockerfile + https

--- node/app/app.js
+++ node/app/app.js
@@ -1,18 +1,18 @@
-// app.js
+// app.js;
 const path = require("path");
 const fs = require("fs");
 const fastify = require("fastify");
 
 // Charger les clés SSL
-// const httpsOptions = {
-//   key: fs.readFileSync("/etc/node/ssl/key.key"),
-//   cert: fs.readFileSync("/etc/node/ssl/key.crt"),
-// };
+const httpsOptions = {
+  key: fs.readFileSync("/etc/node/ssl/key.key"),
+  cert: fs.readFileSync("/etc/node/ssl/key.crt"),
+};
 
 // Créer un serveur Fastify en HTTPS
 const app = fastify({
   logger: true,
-  // https: httpsOptions,
+  https: httpsOptions,
 });
 
 // Exemple de route
@@ -21,7 +21,8 @@ app.get("/", async (request, reply) => {
 });
 
 // Démarrer le serveur
-app.listen({ port: 4000 }, (err, address) => {
+const PORT = process.env.PORT;
+app.listen({ port: PORT, host: "0.0.0.0" }, (err, address) => {
   if (err) throw err;
   console.log(`Server listening on ${address}`);
 });
