098a18319dd5967f677aed04b001441c1ae7f6ff
llebioda <llebioda@student.42.fr>
2025-04-05 17:06:46 +0200

    fixed route

--- node/app/server/routes/config.ts
+++ node/app/server/routes/config.ts
@@ -1,12 +1,10 @@
 import { FastifyInstance, FastifyPluginOptions } from 'fastify';
 
 export default async function (app: FastifyInstance, opts: FastifyPluginOptions): Promise<void> {
-  app.get("/", (request, reply) => {
+  app.get("/config", (request, reply) => {
     reply.send({
       domainName: process.env.DOMAIN_NAME,
-      port: process.env.PORT,
-      db_dir: process.env.DB_DIR,
-      db_path: process.env.DB_PATH,
+      port: process.env.PORT
     });
   });
 };
