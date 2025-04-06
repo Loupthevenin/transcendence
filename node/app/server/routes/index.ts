ec4f7864213103e5db24adf68f289b67588c70d0
Loup Thevenin <loup.thevenin.1@gmail.com>
2025-04-06 09:49:12 +0200

    add tailwindcss

--- node/app/server/routes/index.ts
+++ node/app/server/routes/index.ts
@@ -1,9 +1,13 @@
-import { FastifyInstance, FastifyPluginOptions } from 'fastify';
-import path from 'path';
+import { FastifyInstance, FastifyPluginOptions } from "fastify";
+import path from "path";
 
 // Serve static files (client-side code)
-export default async function (app: FastifyInstance, opts: FastifyPluginOptions): Promise<void> {
+export default async function (
+  app: FastifyInstance,
+  opts: FastifyPluginOptions,
+): Promise<void> {
   app.register(require("@fastify/static"), {
     root: path.join(__dirname, "../../client"),
   });
 }
+
