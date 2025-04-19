e34176a2f783e1698ececafe40756e96ec2959af
Loup Thevenin <loup.thevenin.1@gmail.com>
2025-04-19 19:45:13 +0200

    Dynamic buttons gameMode with auth

--- frontend/src/layout/mode.ts
+++ frontend/src/layout/mode.ts
@@ -4,6 +4,7 @@ export function ModeLayout(): HTMLElement {
   const container: HTMLElement = document.createElement("div");
   container.className =
     "absolute right-8 top-1/2 transform -translate-y-1/2 flex flex-col space-y-4 z-1001";
+  container.id = "menu-mode";
 
   container.appendChild(ButtonMode("singleplayer"));
   container.appendChild(ButtonMode("local"));
