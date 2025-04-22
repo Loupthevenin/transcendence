d36922ae3516aff8f1f7ca6eea7c6f40b755f720
llebioda <llebioda@student.42.fr>
2025-04-22 14:58:50 +0200

    beautify debug database API output and updated .gitignore to ignore all uploaded avatars files

--- backend/app/src/controllers/dbController.ts
+++ backend/app/src/controllers/dbController.ts
@@ -7,23 +7,17 @@ export async function listUsers(
   reply: FastifyReply
 ): Promise<void> {
   const stmt: Statement = db.prepare("SELECT * FROM users");
-  const users: Array<Record<string, any>> = stmt.all() as Array<
-    Record<string, any>
-  >;
-  return reply.send(users);
+  const users: Array<Record<string, any>> = stmt.all() as Array<Record<string, any>>;
+
+  return reply.send(JSON.stringify(users, null, 2));
 };
 
 export async function listMatchHistory(
   request: FastifyRequest,
   reply: FastifyReply
 ): Promise<void> {
-  const stmt: Statement = db.prepare(`
-    SELECT * FROM match_history ORDER BY date DESC
-  `);
+  const stmt: Statement = db.prepare(`SELECT * FROM match_history ORDER BY date DESC`);
+  const matchHistory: Array<Record<string, any>> = stmt.all() as Array<Record<string, any>>;
 
-  const matchHistory: Array<Record<string, any>> = stmt.all() as Array<
-    Record<string, any>
-  >;
-
-  return reply.send(matchHistory);
+  return reply.send(JSON.stringify(matchHistory, null, 2));
 };
