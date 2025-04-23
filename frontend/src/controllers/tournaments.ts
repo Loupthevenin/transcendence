938b227547512b298c44b2c76ff83c8896330ddd
Loup Thevenin <loup@mac.home>
2025-04-23 19:56:47 +0200

    set displayName in all input tournaments

--- frontend/src/controllers/tournaments.ts
+++ frontend/src/controllers/tournaments.ts
@@ -1,1 +1,69 @@
-export function tournamentsHandlers(container: HTMLElement): void {}
+import { UserProfile } from "./types";
+
+async function setDisplayNameInputs(): Promise<void> {
+  const token: string | null = localStorage.getItem("auth_token");
+  if (!token) return;
+
+  try {
+    const res: Response = await fetch("/api/profile", {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+    if (!res.ok) return;
+
+    const data: UserProfile = await res.json();
+    if (data?.name) {
+      const displayInputs: NodeListOf<HTMLInputElement> =
+        document.querySelectorAll<HTMLInputElement>(
+          'input[name="displayName"]',
+        );
+      displayInputs.forEach((input) => {
+        input.value = data.name;
+      });
+    }
+  } catch (err) {
+    console.error("Error loading name : ", err);
+  }
+}
+
+// async function loadTournaments(): Promise<void> {
+//   const token: string | null = localStorage.getItem("auth_token");
+//   if (!token) {
+//     alert("Pas de token !");
+//     return;
+//   }
+//   try {
+//     // const res: response = await fetch("/api/tournaments", )
+//   } catch (err) {
+//     console.error("Error loading Tournaments", err);
+//   }
+// }
+
+function createTournament(): void {
+  const buttonCreateTournament: HTMLButtonElement | null =
+    document.getElementById("create-tournament") as HTMLButtonElement;
+  const form: HTMLFormElement | null = document.getElementById(
+    "tournament-form",
+  ) as HTMLFormElement;
+  if (!buttonCreateTournament || !form) return;
+
+  buttonCreateTournament.addEventListener("click", () => {
+    form.classList.remove("hidden");
+    buttonCreateTournament.classList.add("hidden");
+  });
+
+  form.addEventListener("reset", () => {
+    form.classList.add("hidden");
+    buttonCreateTournament.classList.remove("hidden");
+  });
+}
+// function joinTournament(): void {}
+
+export function tournamentsHandlers(container: HTMLElement): void {
+  // loadTournament();
+  setDisplayNameInputs();
+  createTournament();
+  // joinTournament();
+}
